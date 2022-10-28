eae06a14a09d1040295926d27e30b13ebf12f585
Utsa-v <114085787+Utsa-v@users.noreply.github.com>
2022-10-28 17:02:44 +0530

    Update Question2.java

--- Question2.java
+++ Question2.java
@@ -1,44 +1,64 @@
 package Assignment3;
 
 import java.util.List;
-
-import javax.swing.text.html.HTMLDocument.Iterator;
+import java.util.concurrent.TimeUnit;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.interactions.Actions;
-import org.testng.annotations.BeforeClass;
-import org.testng.annotations.Test;
+import org.testng.Assert;
+import org.testng.annotations.*;
+
+//import io.github.bonigarcia.wdm.WebDriverManager;
+
+
 
 public class Question2 {
-	public WebDriver driver;
+    public WebDriver driver;
+    public Actions action;
 
-	@BeforeClass()
-	public void beforeclass() {
-		System.setProperty("webdriver.chrome.driver",
-				"C:\\Users\\Utsav Sharma\\eclipse-workspace\\SeleniumAssignment3\\src\\test\\resources\\chromedriver.exe");
-		driver = new ChromeDriver();
-		driver.manage().window().maximize();
-		driver.get("https://www.tothenew.com/");
-	}
+    @BeforeMethod
+    public void Browser() {
+    	System.setProperty("webdriver.chrome.driver","C:\\Users\\Utsav Sharma\\eclipse-workspace\\SeleniumAssignment3\\src\\test\\resources\\chromedriver.exe");
+        driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.navigate().to(" https://www.tothenew.com/");
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+        action = new Actions(driver);
+    }
 
-	@Test(priority = 1)
-	public void navabr() throws InterruptedException {
-		
-	WebElement text = driver.findElement(By.xpath("(//a[@id=\"h-Services\"])[2]"));
-	Actions action = new Actions(driver);
-	action.moveToElement(text).perform();
-	System.out.println("Action Performed");
-	Thread.sleep(2000);
-	List <WebElement> list = driver.findElements(By.xpath("(//li[@class=\"menu-item menu-expanded\"]//ul)[3]//li//a"));
-   for(int i=0; i<list.size(); i++) {
-			WebElement e = list.get(i);
-			System.out.println(e.getText());
-			
-		}
- 
-		
-	}
+    @Test
+    public void Verify_Options_in_Service_DropDown() {
+        action = new Actions(driver);
+        WebElement service = driver.findElement(By.xpath("(//a[@id='h-Services'])[2]"));
+        action.moveToElement(service).perform();
+        List<WebElement> ServiceDropDown = driver
+                .findElements(By.xpath("(//li[@class='menu-item menu-expanded'])[4]//ul/li"));
+        for (int i = 0; i < ServiceDropDown.size(); i++) {
+            System.out.println(ServiceDropDown.get(i).getText());
+            System.out.println("*********************************");
+        }
+        String S1 = ServiceDropDown.get(0).getText();
+        String S2 = ServiceDropDown.get(1).getText();
+        String S3 = ServiceDropDown.get(2).getText();
+        String S4 = ServiceDropDown.get(3).getText();
+        String S5 = ServiceDropDown.get(4).getText();
+        String S6 = ServiceDropDown.get(5).getText();
+
+        Assert.assertEquals(S1, "Digital Engineering");
+        Assert.assertEquals(S2, "Cloud & DevOps");
+        Assert.assertEquals(S3, "OTT Solutions & Services");
+        Assert.assertEquals(S4, "Data & Analytics");
+        Assert.assertEquals(S5, "CX, Content & Commerce");
+        Assert.assertEquals(S6, "Digital Marketing");
+
+    }
+
+    @AfterMethod
+    public void Close() {
+        driver.close();
+    }
+
 }
